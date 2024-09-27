6791cd147ec60c7bb80afae5e805271712e6fb93
Sohaib samad <sohaibsamad.hala@gmail.com>
2024-09-27 21:56:24 +0530

    Render Data from API Simple

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import { useState, useEffect } from "react";
 
-export default  function App() {
-  /* when ever we want to render some data we have to store it in the state first to 
-  retrieve it */
-  const [name, setName] = useState([])
+export default function App() {
+  // First create a state where we can temp store the data
+  const [name, setName] = useState([]);
 
   /* The main purpose of adding names() inside the useEffect() is to trigger the API 
   call when the component is rendered (mounted) for the first time.*/
   useEffect(() => {
-    names()
+    response()
   }, [])
 
-  const names = async () => {
-    const getData = await fetch(`https://fakestoreapi.com/carts`)
-    setName(await getData.json())
+  // Then create an async function to fetch the data
+  const response = async () => {
+    const names = await fetch(`https://jsonplaceholder.typicode.com/albums/`)
+    setName(await names.json())
   }
 
   return (
     <div>
-      <ol>
+      <h1>Albums: </h1>
+      <ol className="list-decimal">
         {
           name.map((data) => {
             return (
-              <li className="space-y-2" key={data.id} >
-                <p>UserID: {data.userId}</p>
-                <p>Date: {new Date(data.date).toDateString()}</p>
-
-                <ul>
-                  {
-                    data.products.map((product) => {
-                      return (
-                        <li key={product.productId}>
-                          <p>ProductID: {product.productId}</p>
-                          <p>Product Quantity: {product.quantity}</p>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
+              <li className="" key={data.id}>
+                <p className="">Title: {data.title}</p>
+                <p className="">UserId: {data.userId}</p>
               </li>
             )
           })
