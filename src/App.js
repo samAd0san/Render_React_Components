39199d16deb3ae7b93985e0b1d44682e40db90d6
Sohaib samad <sohaibsamad.hala@gmail.com>
2024-10-16 22:25:28 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
+      {/* To Stick the Footer at the bottom */}
+      <main className="flex-grow">
+        {/* Your main content goes here */}
+      </main>
+      <Footer />
     </div>
   )
 }
